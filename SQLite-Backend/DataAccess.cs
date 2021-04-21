19985422bec1323d2d1dc5c08f7fbec0998d9ffc
Saltpile123 <ikhoudvanberichten@gmail.com>
2021-04-21 16:56:15 +0200

    Add project files.

---
+++ SQLite-Backend/DataAccess.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace SQLite_Backend
+{
+    public class DataAccess
+    {
+        private readonly string dbpath;
+        private readonly string password;
+
+        private string connectionString;
+
+        public DataAccess(string _dbpath, string _password = "HelloWorld")
+        {
+            dbpath = _dbpath;
+            password = _password;
+            connectionString = new SqliteConnectionStringBuilder
+            {
+                DataSource = dbpath,
+                Password = password,
+                ForeignKeys = false // Enable or disable whenever you wanna use it or not.
+            }.ToString();
+        }
+
+        public bool CreateDatabase()
+        {
+            try
+            {
+                if (File.Exists(dbpath))
+                {
+                    Console.WriteLine("Database already exists.");
+                    return false;
+                }
+
+                using (SqliteConnection con = new SqliteConnection(connectionString)) // Creating a new SqliteConnection on a (big) encrypted database might take some time, if there are many database calls, the connection should be made once and kept in memory.
+                using (SqliteCommand cmd = new SqliteCommand
+                (
+                    commandText:"CREATE TABLE[TestTable] (" +
+                                  "[Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                                  "[Data] text NULL);",
+                    connection:con
+                ))
+                {
+                    con.Open();
+
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Database created.");
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public string[] RunQuery(string query)
+        {
+            try
+            {
+                List<string> results = new List<string>();
+                using (SqliteConnection con = new SqliteConnection(connectionString))
+                using (SqliteCommand cmd = new SqliteCommand(
+                    commandText: query,
+                    connection: con))
+                {
+                    con.Open();
+
+                    using (SqliteDataReader reader = cmd.ExecuteReader())
+                        while (reader.Read())
+                        {
+                            object[] res = new object[reader.FieldCount];
+                            reader.GetValues(res);
+                            results.Add(string.Join("\t", res));
+                        }
+                    return results.ToArray();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+                Console.WriteLine(e.Message);
+                return new string[] { "Error" };
+            }
+        }
+    }
+}
---
+++ UWP-Encrypted-SQLite/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using SQLite_Backend;
+using System.IO;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace UWP_Encrypted_SQLite
+{
+    public sealed partial class MainPage : Page
+    {
+        string dbpath = ApplicationData.Current.LocalFolder.Path + "\\testdb.db";
+        DataAccess dbAccess;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            dbAccess = new DataAccess(dbpath);
+        }
+
+        private void CreateDatabase_Click(object sender, RoutedEventArgs e)
+        {
+            if (dbAccess.CreateDatabase())
+                CreateDBresBlock.Text = "Database created...";
+            else
+                CreateDBresBlock.Text = "Database creation failed, check console...";
+        }
+
+        private void DeleteDatabase_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(dbpath))
+                File.Delete(dbpath);
+            CreateDBresBlock.Text = "Database deleted...";
+        }
+
+        private void RunQuery_Click(object sender, RoutedEventArgs e)
+        {
+            QueryResultView.ItemsSource = dbAccess.RunQuery(Querybox.Text);
+        }
+    }
+}
